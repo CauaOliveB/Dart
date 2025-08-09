a0e17462b3d9a83f30d8a9f02d7cb7e749af353f
CauaOliveB <cauaolivebarbosa@gmail.com>
2025-08-08 23:45:08 -0300

    Market Completed

--- 2°Class/Exercices/market.dart
+++ 2°Class/Exercices/market.dart
@@ -1,6 +1,6 @@
 import "dart:io";
 
-void main(){
+void main() {
   print("""HELLO, WELCOME TO DANDARA MARKET.
   
   PRODUCTS
@@ -13,50 +13,70 @@ void main(){
   [7] Chocolate
   \n
   """);
+
   int moreItems = 0;
+  List<Map<String, dynamic>> myCart = [];
 
-  stdout.write("What is your CPF:");
-  dynamic? cpf = stdin.readLineSync();
+  stdout.write("What is your CPF: ");
+  String cpf = stdin.readLineSync()!;
 
-  do{
-    stdout.write("Insert the name of a product you want to buy:");
-  dynamic? productName = stdin.readLineSync();
+  do {
+    stdout.write("Insert the name of a product you want to buy: ");
+    String productName = stdin.readLineSync()!;
 
-  stdout.write("Insert the price of a product you want to buy:");
-  dynamic? producPrice = stdin.readLineSync();
+    stdout.write("Insert the price of a product you want to buy: ");
+    double productPrice = double.parse(stdin.readLineSync()!);
 
-  dynamic productNamePrice = ("[$productName] - BRL $producPrice.");
+    myCart.add({"name": productName, "price": productPrice});
 
-  print("""You wish to include more itens?
-  
+    print("""
+You wish to include more items?
     [1] Yes
     [2] No
-
-  """);
-  moreItems = int.parse(stdin.readLineSync()!);
+""");
+    moreItems = int.parse(stdin.readLineSync()!);
 
   } while (moreItems == 1);
-    
-  
 
+  if (moreItems == 2) {
+    print("\nProducts:");
 
-  switch (moreItems){
-    case 1 :
-    while (moreItems == 1) {
-        stdout.write("Insert the name of a product you want to buy:");
-        dynamic? productName = stdin.readLineSync();
-
-        stdout.write("Insert the price of a product you want to buy:");
-        dynamic? producPrice = stdin.readLineSync();
-
-        dynamic productNamePrice = ("[$productName] - BRL $producPrice.");
-
-        
+    double total = 0;
+    int index = 0;
+    while (index < myCart.length) {
+      print("- ${myCart[index]["name"]}: R\$ ${myCart[index]["price"]}");
+      total += myCart[index]["price"];
+      index++;
     }
+    print("TOTAL: R\$ ${total.toStringAsFixed(2)}\n");
 
+    print("""Choose your option of payment:
+    [1] Cash Payment - (10% discounts)
+    [2] Pay in installments - (10% interest)
+    [3] To pay in the next time at buy - (15% interest)
+    """);
+
+    int paymentMethod = int.parse(stdin.readLineSync()!);
+
+    switch (paymentMethod) {
+      case 1:
+        double finalValue = total - (total * 0.10);
+        print("You have to pay R\$ ${finalValue.toStringAsFixed(2)} (10% discount).");
+        break;
+
+      case 2:
+        double installment = total / 2;
+        print("You have to pay 2 installments of R\$ ${installment.toStringAsFixed(2)}.");
+        break;
+
+      case 3:
+        double finalValue = total + (total * 0.10);
+        double installment = finalValue / 3;
+        print("You have to pay 3 installments of R\$ ${installment.toStringAsFixed(2)}, total R\$ ${finalValue.toStringAsFixed(2)}.");
+        break;
+
+      default:
+        print("Invalid Option");
+    }
   }
-  
-  
-
-  
 }
