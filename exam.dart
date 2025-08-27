801923afd3e9970a4eca4f27bde5ce91f51fb3a8
Joao-Adegas <pedrojoao6378@gmail.com>
2025-08-27 09:23:35 -0300

    Exam list<map<string, dynamic>> test

---
+++ exam.dart
@@ -0,0 +1,16 @@
+import "dart:io";
+
+void main() {
+
+  print("""Hello, good morning!
+        Welcome to PharmaHelp""")
+
+  List<Map<String, dynamic, int>> myCart = 
+  [{'Antacid' : [8.00]}, 
+  {'Diapers' :[16.00]}, 
+  {'Antihistamine' : [22.00]}, 
+  {'Burn Ointment' : [17.00]}, ];
+}
+
+
+
