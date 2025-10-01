5dfb2ee01766ab6f96160f7c2e55b6c45a663e03
CauaOliveB <cauaolivebarbosa@gmail.com>
2025-10-01 03:40:41 -0300

    Customar register + Customer Validation + Show Products + Cart

--- 6°Class/exam.dart
+++ 6°Class/exam.dart
@@ -8,54 +8,54 @@ void main() {
 
 }
 
-//Customer Registration
+//==============Customer Registration==============
 
-String CustomerRegister() {
+  Map<String, String> CustomerRegister() {
+    print("Please enter your name:");
+    String name = stdin.readLineSync()!;
 
-  print("Please enter your name:");
-  String? customerName = stdin.readLineSync()!;
-
-  print("Please enter your CPF:");
-  String? cpf = stdin.readLineSync()!;
-
-  return("Name: $customerName | CPF:$cpf ");
+    print("Please enter your CPF:");
+    String cpf = stdin.readLineSync()!;
+    
+    return {"name": name, "cpf": cpf};
 }
 
-//Customer Validation
 
-String CustomerValidation(String document){ 
-  
+//==============Customer Validation==============
+
+String CustomerValidation(){ 
+
     do{
-      try {
-      CustomerRegister(customerName, cpf);
-      } on FormatException{
-        print("Customer name cannot be empty, null or have spaces.");
-      } catch(e){
-        print("Your error is $e");
+      
+      var data = CustomerRegister();
+
+      if (data["name"]!.trim().isEmpty || data["cpf"]!.trim().isEmpty) {
+      print("Name and CPF cannot be empty. Please try again.");
       }
 
-      try {
-        print("Please enter your CPF:");
-        String? document = stdin.readLineSync()!;
-      } on FormatException{
-        print("Document cannot be empty, null or have spaces.");
-      }catch(e){
-        print("Your error is $e");
-      }
-    }while(customerName == null || customerName.trim().isEmpty || document == null || document.trim().isEmpty);
+    }while(data["name"]!.trim().isEmpty || data["cpf"]!.trim().isEmpty);
 
-    return("Client : ");
+return "Name: ${data["name"]} | CPF: ${data["cpf"]}";
 }
 
-dynamic Cart() {
+//==============Show Products==============
+
+dynamic ShowProducts() {
+
+    //Name : String | Price : double | Stock : int
   List<Map<String, dynamic>> products = 
   [{'name' : 'Antacid', 'price' : 8.00, 'stock' : 8}, 
   {'name' : 'Diapers', 'price' : 16.00, 'stock' : 36}, 
   {'name' : 'Antihistamine', 'price' : 22.00,'stock' : 16}, 
   {'name' : 'Burn Ointment', 'price' : 17.00, 'stock' : 13}];
 
-  //Name : String | Price : double | Stock : int
+  for (int i = 0; i < products.length; i++) {
+        print("$i - ${products[i]["name"]} | BRL ${products[i]["price"].toStringAsFixed(2)} | Stock: ${products[i]["stock"]}");
+      }
 
+//==============Cart==============
+
+dynamic Cart() {
   List <Map<String, dynamic>> myCart = [];
 
   print("""Let's start buying?
@@ -63,12 +63,12 @@ dynamic Cart() {
   [2]No
   """);
 
-  int buying = int.parse(stdin.readLineSync()!);
+  String shopping = stdin.readLineSync()!;
+  bool shoppingBoolean = false;
 
-  if (buying == 1){
-    for (int i = 0; i < products.length; i++) {
-      print("$i - ${products[i]["name"]} | BRL ${products[i]["price"].toStringAsFixed(2)} | Stock: ${products[i]["stock"]}");
-    }
+  if (shopping == "1"){
+    
+    shoppingBoolean = true;
 
     do {
       print("Enter the product number:");
@@ -88,25 +88,32 @@ dynamic Cart() {
       } 
       else {
         double total = products[option]['price'] *  quantity; 
-        myCart.add("$option - $quantity | Total $total : " as Map<String, dynamic>);
+        
+        myCart.add({
+        "product": products[option]["name"],
+        "quantity": quantity,
+        "total": total});
+
         products[option]["stock"] -= quantity;
       }
+
       print("""Let's continue buying?
       [1]Yes
       [2]No
       """);
       
-      buying;
+      String shopping = stdin.readLineSync()!;
       
-    } while (buying == 1);
-    
+    } while (shopping == "1");
+}
+  
     } else {
       print("=========================");
       print("Thank you for your preference!");
       print("=========================");
     }
   }
-}
+
 
 
 dynamic paymentMethod() {
@@ -123,5 +130,31 @@ dynamic paymentMethod() {
     } on RangeError {
       print("Select a payment method valid.");
     }
+/*
+    String paymentMethod = int.parse(stdin.readLineSync()!);
+
+    switch (paymentMethod) {
+      case 1:
+        double finalValue = total - (total * 0.10);
+        print("You have to pay R\$ ${finalValue.toStringAsFixed(2)} (10% discount).");
+        break;
+
+      case 2:
+        double installment = total / 2;
+        print("You have to pay 2 installments of R\$ ${installment.toStringAsFixed(2)}.");
+        break;
+
+      case 3:
+        double finalValue = total + (total * 0.10);
+        double installment = finalValue / 3;
+        print("You have to pay 3 installments of R\$ ${installment.toStringAsFixed(2)}, total R\$ ${finalValue.toStringAsFixed(2)}.");
+        break;
+
+      case 4:
+
+      default:
+        print("Invalid Option");
+    }
+    */
 }
 
